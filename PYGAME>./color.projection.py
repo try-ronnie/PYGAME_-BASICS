392b5072c5771864922e2326f1c96782e19a6bb6
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-01-26 12:09:37 +0300

    shape creation on gui

--- PYGAME>./color.projection.py
+++ PYGAME>./color.projection.py
@@ -3,8 +3,8 @@ import pygame , sys
 from pygame.locals import *
 
 pygame.init() #
-DISPLAYSURF = pygame.display.set_mode((500.500))
-Name = pygame.display.set_caption()
+DISPLAYSURF = pygame.display.set_mode((1000, 1000))
+pygame.display.set_caption("surface game")
 # setting default display colours
 #we also need to realise :
     #tuple for display for positional value is 2 (pygames cartesian plane)
@@ -26,15 +26,26 @@ DISPLAYSURF.fill(WHITE)
 
 #drawing the objects 
 # pygame.draw.polygon (surface , colour , pointlist , width(thickness))
-pygame.draw.line(DISPLAYSURF, BLACK ,(69,50) , (120,60) , 4)
-pygame.draw.rect(DISPLAYSURF , RED , (180,210) ())
+pygame.draw.line(DISPLAYSURF, BLACK ,(69,50 ), (120,60) , 4)
+pygame.draw.rect(DISPLAYSURF , RED , (180,9 ,60,90))
+pygame.draw.circle(DISPLAYSURF, WHITE , (70,70) , 8 , 10)
+pygame.draw.ellipse(DISPLAYSURF , GREEN , (250 , 400 , 100, 100),5)
+pygame.draw.rect (DISPLAYSURF, RED , (449 , 60 , 90, 188), 8)
 
-
+pixobj = pygame.PixelArray(DISPLAYSURF)
+pixobj[990] [380] = RED
+pixobj[991] [382] = GREEN
+pixobj[992] [384] = BLACK
+pixobj[995] [386] = BLACK
+del pixobj
 
 running = True # true value
 while running:
     for event in pygame.event.get():
-        if event == QUIT:
+        if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    
+    pygame.display.update()# should be out so that it updates every time the loop ends
+        
         
